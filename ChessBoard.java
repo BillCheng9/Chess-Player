d5a71e46cdbc3b8b5045d293f73170dfddfddf29
yifanww <112586920+yifanww@users.noreply.github.com>
2024-02-07 18:06:15 -0500

    Create ChessBoard.java

---
+++ ChessBoard.java
@@ -0,0 +1,137 @@
+import java.util.Arrays;
+
+public class ChessBoard {
+    private long WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK;
+    ChessBoard(String fen){
+        String[] fenParts = fen.split(" ");
+        setBoard(fenParts[0]);
+    }
+    public void setBoard(String position){
+        int rank = 7;
+        int file = 0;
+
+        for (char c:position.toCharArray()){
+            if(Character.isDigit(c)){
+                file += Character.getNumericValue(c);
+            } else if (c == '/') {
+                rank--;
+                file = 0;
+            } else {
+                long square = 1L << (rank * 8 + file);
+                switch (c) {
+                    case 'P' -> WP |= square;
+                    case 'N' -> WN |= square;
+                    case 'B' -> WB |= square;
+                    case 'R' -> WR |= square;
+                    case 'Q' -> WQ |= square;
+                    case 'K' -> WK |= square;
+                    case 'p' -> BP |= square;
+                    case 'n' -> BN |= square;
+                    case 'b' -> BB |= square;
+                    case 'r' -> BR |= square;
+                    case 'q' -> BQ |= square;
+                    case 'k' -> BK |= square;
+                }
+                file++;
+            }
+        }
+    }
+    public long getWP() {
+        return WP;
+    }
+
+    public long getWN() {
+        return WN;
+    }
+
+    public long getWB() {
+        return WB;
+    }
+
+    public long getWR() {
+        return WR;
+    }
+
+    public long getWQ() {
+        return WQ;
+    }
+
+    public long getWK() {
+        return WK;
+    }
+
+    public long getBP() {
+        return BP;
+    }
+
+    public long getBN() {
+        return BN;
+    }
+
+    public long getBB() {
+        return BB;
+    }
+
+    public long getBR() {
+        return BR;
+    }
+
+    public long getBQ() {
+        return BQ;
+    }
+
+    public long getBK() {
+        return BK;
+    }
+    public static void drawArray(long WP, long WN, long WB, long WR, long WQ, long WK,
+                                 long BP, long BN, long BB, long BR, long BQ, long BK) {
+        String chessBoard[][] = new String[8][8];
+        for (int i = 0; i < 64; i++) {
+            chessBoard[7 - i / 8][i % 8] = " "; // Reverse the rank
+        }
+        for (int i = 0; i < 64; i++) {
+            if (((WP >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "P";
+            }
+            if (((WN >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "N";
+            }
+            if (((WB >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "B";
+            }
+            if (((WR >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "R";
+            }
+            if (((WQ >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "Q";
+            }
+            if (((WK >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "K";
+            }
+            if (((BP >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "p";
+            }
+            if (((BN >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "n";
+            }
+            if (((BB >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "b";
+            }
+            if (((BR >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "r";
+            }
+            if (((BQ >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "q";
+            }
+            if (((BK >> i) & 1) == 1) {
+                chessBoard[7 - i / 8][i % 8] = "k";
+            }
+        }
+        for (int i = 0; i < 8; i++) {
+            System.out.println(Arrays.toString(chessBoard[i]));
+        }
+    }
+    public void drawBoard() {
+        ChessBoard.drawArray(WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK);
+    }
+}
