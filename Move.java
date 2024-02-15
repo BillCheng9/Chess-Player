87b2db9ba94a38a6bd56ca0556ddd1921ce3ce02
Bill <ninjabill321@gmail.com>
2024-02-14 19:12:46 -0500

    changed k vars to n to avoid king var clash

--- Move.java
+++ Move.java
@@ -1,22 +1,22 @@
 public class Move {
     final private static long FILE_GH=217020518514230019L;
     final private static long FILE_AB=-4557430888798830400L;
-    public static long getWKnightMoves(long k, long w){
-        long km = 0L;
-        while(k!=0L) {
-            long m = Long.highestOneBit(k);
-            long fkm = (m << 6 | m << 10 | m << 15 | m << 17 | m >> 6 | m >> 10) & (~w);
+    public static long getWKnightMoves(long n, long w){
+        long nm = 0L;
+        while(n!=0L) {
+            long m = Long.highestOneBit(n);
+            long fnm = (m << 6 | m << 10 | m << 15 | m << 17 | m >> 6 | m >> 10) & (~w);
             int n_zero = Long.numberOfTrailingZeros(m);
             if (n_zero%8 < 4) {
-                fkm &= (~w)&(~FILE_AB);
+                fnm &= (~w)&(~FILE_AB);
             }
             else {
-                fkm &= (~w)&(~FILE_GH);
+                fnm &= (~w)&(~FILE_GH);
             }
-            km |= fkm;
-            k = (~m) & k;
+            nm |= fnm;
+            n = (~m) & n;
         }
-        return km;
+        return nm;
     }
 
     public long getWPawnMoves(long wPawns, long wPieces, long bPieces){
